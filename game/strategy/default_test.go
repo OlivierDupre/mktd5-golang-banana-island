593ae3be188643d2b1993064e5ffe23c6fa258ab
Olivier Dupré <odupre@astek.fr>
2017-11-24 15:27:42 +0100

    Improved WhatsAroundMe testing v2

--- game/strategy/default_test.go
+++ game/strategy/default_test.go
@@ -29,7 +29,16 @@ func TestWhatsAroundMe(t *testing.T) {
 		t.Errorf("West should be 3, got %v", result[mediator.West])
 	}
 
-	result = strategy.WhatsAroundMe(0,0, sampleMap)
+}
+
+func TestWhatsAroundMe1(t *testing.T) {
+	sampleMap := [][]mediator.Cell{
+		{0,1,0},
+		{3,0,2},
+		{0,0,0},
+	}
+
+	result := strategy.WhatsAroundMe(0,0, sampleMap)
 	if result[mediator.North] != 2 {
 		t.Errorf("North should be 2, got %v", result[mediator.North])
 	}
@@ -41,5 +50,28 @@ func TestWhatsAroundMe(t *testing.T) {
 	}
 	if result[mediator.West] != 2 {
 		t.Errorf("West should be 2, got %v", result[mediator.West])
+	}
+}
+
+func TestWhatsAroundMe2(t *testing.T) {
+	sampleMap := [][]mediator.Cell{
+		{0,1,0},
+		{3,0,2},
+		{0,0,0},
+	}
+
+
+	result := strategy.WhatsAroundMe(3,3, sampleMap)
+	if result[mediator.North] != 2 {
+		t.Errorf("North should be 2, got %v", result[mediator.North])
+	}
+	if result[mediator.East] != 2 {
+		t.Errorf("East should be 2, got %v", result[mediator.East])
+	}
+	if result[mediator.South] != 2 {
+		t.Errorf("South should be 2, got %v", result[mediator.South])
+	}
+	if result[mediator.West] != 0 {
+		t.Errorf("West should be 0, got %v", result[mediator.West])
 	}
 }
