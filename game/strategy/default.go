24037984f68cb041516b17bd2e14dff7adb8864c
Olivier Dupré <odupre@astek.fr>
2017-11-24 14:37:19 +0100

    Added random strategy and console map printing

--- game/strategy/default.go
+++ game/strategy/default.go
@@ -3,6 +3,8 @@ package strategy
 import (
 	"mktd5/mktd-island/client/log"
 	"mktd5/mktd-island/client/mediator"
+	"math/rand"
+	"fmt"
 )
 
 type DefaultMoveStrategy struct {
@@ -22,5 +24,46 @@ type DefaultMoveStrategy struct {
 // for the current turn and the error will be logged.
 func (d *DefaultMoveStrategy) DecideWhereToGo(helper Helper) (mediator.Direction, error) {
 	d.Logger.Info("let's go!", nil)
-	return mediator.None, nil
+
+	//rand.Seed(42) // If you fix this number, the random will always start at the same number
+	newDirection := randomMove()
+
+	//gameState := helper.GameState()
+	printMap(helper)
+
+	return newDirection, nil
+}
+
+func randomMove() mediator.Direction{
+	directions := []mediator.Direction{
+		mediator.North,
+		mediator.East,
+		mediator.South,
+		mediator.West,
+	}
+
+	return directions[rand.Intn(len(directions))]
+}
+
+func printMap(helper Helper) {
+	state := helper.GameState()
+	for y := 0; y < 16; y++ {
+		for x := 0; x < 16; x++ {
+			cell, _ := state.Map.Cell(x, y)
+			if cell.Banana() {
+				fmt.Print("b")
+			} else if helper.IsMe(cell) {
+				fmt.Print("M")
+			} else if cell.Player() {
+				fmt.Print("m")
+			} else if cell.Wall() {
+				fmt.Print(".")
+			} else if cell.Empty() {
+				fmt.Print(" ")
+			} else {
+				fmt.Print("?")
+			}
+		}
+		fmt.Println("")
+	}
 }
